627facbad35a5033663e3f9483e91b41f3814121
serrodcal <sergiorodriguezcalvo@gmail.com>
2016-11-17 11:17:01 +0100

    Created method to implement the logic

--- apig.py
+++ apig.py
@@ -7,12 +7,19 @@ def get_dict_from_json_file(file):
     json_data = open(file).read()
     return json.loads(json_data)
 
+def create_api_from_specification(api_specification):
+    return list() #TODO
+
+def generate_result_file(api_result):
+    print("File has been created!") #TODO
+
 if __name__ == '__main__':
     if len(sys.argv)>1:
         file = sys.argv[1]
         if existsFile(file):
             print("Correct File!") #TODO: remove this statements when existsFile method is completed.
             api_specification = get_dict_from_json_file(file)
-            print(api_specification)
+            api_result = create_api_from_specification(api_specification)
+            generate_result_file(api_result)
         else:
             print("File argument is not valid!")
