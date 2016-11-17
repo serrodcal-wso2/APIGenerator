e64ea0fc8cb06d27aac5a38ccd3ef4b93ddde00d
serrodcal <sergiorodriguezcalvo@gmail.com>
2016-11-17 14:20:25 +0100

    Generated file functionality implemented

--- apig.py
+++ apig.py
@@ -7,6 +7,36 @@ def get_dict_from_json_file(file):
     json_data = open(file).read()
     return json.loads(json_data)
 
+def get_methods(methods):
+    return "GET POST"
+
+def get_string_from_list(list):
+    result = ""
+    for item in list:
+        result += item
+    return result
+
+def create_resource_from_specification(resource):
+    result = list()
+    
+    methods_statement = get_methods(resource['methods'])
+
+    initial_resource_statement = "<resource methods=\"" + methods_statement + "\" uri-template=\"" + resource['urlPath'] + "\">"
+    result.append(initial_resource_statement)
+
+    resource_content = ""
+    if "conf" in resource['endpoint']:
+        resource_content = "<inSequence><send><endpoint key=\"" + resource['endpoint'] + "\" /></send></inSequence><outSequence><send /><outSequence><faultSequence/>"
+    else:
+        resource_content = "<inSequence><send><endpoint uri=\"" + resource['endpoint'] + "\" /></send></inSequence><outSequence><send /><outSequence><faultSequence/>"
+    result.append(resource_content)
+
+    result.append("</resource>")
+
+    result_string = get_string_from_list(result)
+
+    return result_string
+
 def create_api_from_specification(api_specification):
     result = list()
 
@@ -14,7 +44,10 @@ def create_api_from_specification(api_specification):
     result.append(initial_api_statement)
 
     for resource in api_specification['resources']:
-        print(resource)
+        #print(resource) #TODO: Remove this
+        resource_result = create_resource_from_specification(resource)
+        result.append(resource_result)
+        print(resource_result)
     
     result.append("</api>")
 
@@ -22,7 +55,11 @@ def create_api_from_specification(api_specification):
 
 def generate_result_file(api_result):
     print("File has been created!") #TODO
-    print(api_result)
+    api_string = get_string_from_list(api_result)
+    text_file = open("output.xml", "w")
+    text_file.write("<?xml version=\"1.0\" encoding=\"utf-8\"?>")
+    text_file.write(api_string)
+    text_file.close()
 
 if __name__ == '__main__':
     if len(sys.argv)>1:
@@ -34,3 +71,14 @@ if __name__ == '__main__':
             generate_result_file(api_result)
         else:
             print("File argument is not valid!")
+
+
+
+
+
+
+
+
+
+
+
