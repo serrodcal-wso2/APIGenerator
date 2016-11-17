117451292a5d5089b4435af868a3a50edaa851d4
serrodcal <sergiorodriguezcalvo@gmail.com>
2016-11-17 11:12:29 +0100

    Main and principal methods to extract dict from json file

--- apig.py
+++ apig.py
@@ -1,4 +1,18 @@
 import sys, os, re, json
 
+def existsFile(file):
+    return True #TODO
+
+def get_dict_from_json_file(file):
+    json_data = open(file).read()
+    return json.loads(json_data)
+
 if __name__ == '__main__':
-    print("Hello, world!")
+    if len(sys.argv)>1:
+        file = sys.argv[1]
+        if existsFile(file):
+            print("Correct File!") #TODO: remove this statements when existsFile method is completed.
+            api_specification = get_dict_from_json_file(file)
+            print(api_specification)
+        else:
+            print("File argument is not valid!")
